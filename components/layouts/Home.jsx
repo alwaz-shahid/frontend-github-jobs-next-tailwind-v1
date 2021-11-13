584c990d750ff4d5be6ae43af4a10552cfeb5b2c
Alwaz18 <52915358+Alwaz18@users.noreply.github.com>
2021-11-13 18:06:27 +0500

    template

--- components/layouts/Home.jsx
+++ components/layouts/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="page" >
-            Home page
+            <h1 className=" text-2xl lg:text-4xl p-20 font-extrabold
+             text-blue-400 underline text-center w-full">
+                Next JS template .ft tailwindcss SWR
+            </h1>
         </div>
     )
 }
